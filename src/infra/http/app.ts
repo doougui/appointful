cbce014ad3b81296d2bf7e19b9622b8cb2a88a46
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2023-03-01 20:07:50 -0300

    feat: adding swagger

--- src/infra/http/app.ts
+++ src/infra/http/app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import { router } from './routes';
 
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from './swagger.json';
+
 const app = express();
 
 app.use(express.json());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(router);
 
 export { app };
