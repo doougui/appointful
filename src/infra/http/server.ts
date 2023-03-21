1cc2e5cfb937959c4b09da0bf168795606e739df
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2023-03-21 18:49:12 -0300

    fix: adjust start script to run from dist and add port to environment variables

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,6 +1,6 @@
 import { app } from './app';
 
-const PORT = 3333;
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3333;
 
 app.listen(PORT, () => {
   console.info(`Server is listening on port ${PORT}`);
