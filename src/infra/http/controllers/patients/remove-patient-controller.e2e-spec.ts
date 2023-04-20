2a862bcbec57befac855763ec12e0047d9c260b4
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2023-04-20 19:17:14 -0300

    making sure it's able to delete a patient

---
+++ src/infra/http/controllers/patients/remove-patient-controller.e2e-spec.ts
@@ -0,0 +1,27 @@
+import { app } from '@infra/http/app';
+import request from 'supertest';
+import { describe, expect, it } from 'vitest';
+
+describe('Remove patient', () => {
+  it('should be able to delete a patient', async () => {
+    const patientResponse = await request(app).post('/patients').send({
+      name: 'Oliver Rodrigo Bernardes',
+      email: 'oliver-bernardes71@keffin.com.br',
+      phone: '+55 55 55555-5555',
+    });
+
+    const response = await request(app).delete(
+      `/patients/${patientResponse.body.id}`,
+    );
+
+    expect(response.statusCode).toBe(204);
+  });
+
+  it('should not be able to delete a non-existing patient', async () => {
+    const response = await request(app).delete(
+      `/patients/non-existing-patient`,
+    );
+
+    expect(response.statusCode).toBe(400);
+  });
+});
